aa17fc0bf5c88b4981bab381f00c29022c7def5e
twinaphex <libretro@gmail.com>
2014-06-30 19:43:12 +0200

    Update libco

--- mednafen/snes/src/lib/libco/libco.c
+++ mednafen/snes/src/lib/libco/libco.c
@@ -4,18 +4,16 @@
   license: public domain
 */
 
-#if defined(__GNUC__) && defined(__i386__)
+#if defined(__GNUC__) && defined(__i386__) || (defined(_MSC_VER) && defined(_M_IX86))
   #include "x86.c"
-#elif defined(__GNUC__) && defined(__amd64__)
+#elif defined(__GNUC__) && defined(__amd64__) || (defined(_MSC_VER) && defined(_M_AMD64))
   #include "amd64.c"
 #elif defined(__GNUC__) && defined(_ARCH_PPC)
   #include "ppc.c"
+#elif defined(__GNUC__) && (defined(__ARM_EABI__) || defined(__arm__))
+  #include "armeabi.c"
 #elif defined(__GNUC__)
   #include "sjlj.c"
-#elif defined(_MSC_VER) && defined(_M_IX86)
-  #include "x86.c"
-#elif defined(_MSC_VER) && defined(_M_AMD64)
-  #include "amd64.c"
 #elif defined(_MSC_VER)
   #include "fiber.c"
 #else
