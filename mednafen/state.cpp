8a7f19830896f587ecbc165a435896435980813e
twinaphex <libretro@gmail.com>
2021-03-24 15:46:59 +0100

    Cleanups

--- mednafen/state.cpp
+++ mednafen/state.cpp
@@ -134,16 +134,11 @@ static bool SubWrite(StateMem *st, SFORMAT *sf, const char *name_prefix = NULL)
       int32_t bytesize = sf->size;
 
       char nameo[1 + 256];
-      int slen;
-
-      slen = snprintf(nameo + 1, 256, "%s%s", name_prefix ? name_prefix : "", sf->name);
+      int slen = snprintf(nameo + 1, 256, "%s%s", name_prefix ? name_prefix : "", sf->name);
       nameo[0] = slen;
 
       if(slen >= 255)
-      {
-         printf("Warning:  state variable name possibly too long: %s %s %s %d\n", sf->name, name_prefix, nameo, slen);
          slen = 255;
-      }
 
       smem_write(st, nameo, 1 + nameo[0]);
       smem_write32le(st, bytesize);
@@ -171,7 +166,6 @@ static bool SubWrite(StateMem *st, SFORMAT *sf, const char *name_prefix = NULL)
          for(int32_t bool_monster = 0; bool_monster < bytesize; bool_monster++)
          {
             uint8_t tmp_bool = ((bool *)sf->v)[bool_monster];
-            //printf("Bool write: %.31s\n", sf->name);
             smem_write(st, &tmp_bool, 1);
          }
       }
@@ -209,11 +203,6 @@ static int WriteStateChunk(StateMem *st, const char *sname, SFORMAT *sf)
 
    memset(sname_tmp, 0, sizeof(sname_tmp));
    memcpy((char *)sname_tmp, sname, (sname_len < 32) ? sname_len : 32);
-
-#ifndef NDEBUG
-   if(sname_len > 32)
-      printf("Warning: section name is too long: %s\n", sname);
-#endif
 
    smem_write(st, sname_tmp, 32);
 
@@ -305,16 +294,10 @@ static int ReadStateChunk(StateMem *st, SFORMAT *sf, int size)
       uint8_t toa[1 + 256];	// Don't change to char unless cast toa[0] to unsigned to smem_read() and other places.
 
       if(smem_read(st, toa, 1) != 1)
-      {
-         puts("Unexpected EOF");
          return(0);
-      }
 
       if(smem_read(st, toa + 1, toa[0]) != toa[0])
-      {
-         puts("Unexpected EOF?");
          return 0;
-      }
 
       toa[1 + toa[0]] = 0;
 
@@ -328,12 +311,8 @@ static int ReadStateChunk(StateMem *st, SFORMAT *sf, int size)
 
          if(recorded_size != expected_size)
          {
-            printf("Variable in save state wrong size: %s.  Need: %d, got: %d\n", toa + 1, expected_size, recorded_size);
             if(smem_seek(st, recorded_size, SEEK_CUR) < 0)
-            {
-               puts("Seek error");
                return(0);
-            }
          }
          else
          {
@@ -361,12 +340,8 @@ static int ReadStateChunk(StateMem *st, SFORMAT *sf, int size)
       }
       else
       {
-         printf("Unknown variable in save state: %s\n", toa + 1);
          if(smem_seek(st, recorded_size, SEEK_CUR) < 0)
-         {
-            puts("Seek error");
             return(0);
-         }
       }
    } // while(...)
 
@@ -404,32 +379,20 @@ int MDFNSS_StateAction(void *st_p, int load, int data_only, std::vector <SSDescr
                if(!strncmp(sname, section->name, 32))
                {
                   if(!ReadStateChunk(st, section->sf, tmp_size))
-                  {
-                     printf("Error reading chunk: %s\n", section->name);
                      return(0);
-                  }
                   found = 1;
                   break;
                } 
                else
                {
                   if(smem_seek(st, tmp_size, SEEK_CUR) < 0)
-                  {
-                     puts("Chunk seek failure");
                      return(0);
-                  }
                }
             }
             if(smem_seek(st, -total, SEEK_CUR) < 0)
-            {
-               puts("Reverse seek error");
                return(0);
-            }
             if(!found && !section->optional) // Not found.  We are sad!
-            {
-               printf("Section missing:  %.32s\n", section->name);
                return(0);
-            }
          }
       }
    }
